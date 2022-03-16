4b9ceff48acab0d5202cabdb55729c641c722b48
Guilherme Polippo <luispolippo@hotmail.com>
2022-03-16 19:43:09 -0300

    Adicionado variavel de ambiente

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const { ENV_MSG } = process.env
 
 app.get('/', (_req, res) => {
-  return res.status(200).send('Está vivo!!!');
+  return res.status(200).send(`Está vivo!!! - Rodando em ${ENV_MSG}`);
 });
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
